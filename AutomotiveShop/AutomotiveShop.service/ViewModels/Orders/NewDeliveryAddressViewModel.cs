b799fdd6b65d89298889a6cc68ff04b850d6bea5
wikpr93 <wikpr93@gmail.com>
2017-11-12 20:10:26 +0100

    ViewModel fix

--- AutomotiveShop/AutomotiveShop.service/ViewModels/Orders/NewDeliveryAddressViewModel.cs
+++ AutomotiveShop/AutomotiveShop.service/ViewModels/Orders/NewDeliveryAddressViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,20 +10,24 @@ namespace AutomotiveShop.service.ViewModels.Orders
     {
         public Guid DeliveryAddressId { get; set; }
 
+        [Display(Name="Company name")]
         public string CompanyName { get; set; }
 
         public string Name { get; set; }
 
         public string Surname { get; set; }
 
+        [Display(Name = "Street name")]
         public string StreetName { get; set; }
 
         public string Postcode { get; set; }
 
         public string City { get; set; }
 
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Additional info")]
         public string AdditionalInfo { get; set; }
     }
 }
