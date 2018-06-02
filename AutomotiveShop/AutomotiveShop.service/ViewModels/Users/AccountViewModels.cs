bb7bc2fb42ceea52b059bb94a21a590d3d8f9b95
Wiktor Baran <wikpr93@gmail.com>
2018-06-02 12:31:23 +0200

    Login + registration

--- AutomotiveShop/AutomotiveShop.service/ViewModels/Users/AccountViewModels.cs
+++ AutomotiveShop/AutomotiveShop.service/ViewModels/Users/AccountViewModels.cs
@@ -72,12 +72,12 @@ namespace AutomotiveShop.service.ViewModels.Users
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Hasło")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Powtórz hasło")]
+        [Compare("Password", ErrorMessage = "Hasła nie pasują do siebie")]
         public string ConfirmPassword { get; set; }
     }
 
